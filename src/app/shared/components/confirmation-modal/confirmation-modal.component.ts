04e6e09019da12c68e878833c8cfb383c4e80f2d
David Ćeranić <david.ceranic@fleksbit.hr>
2020-05-14 13:37:08 +0200

    Add confirmation modal

--- src/app/shared/components/confirmation-modal/confirmation-modal.component.ts
+++ src/app/shared/components/confirmation-modal/confirmation-modal.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-confirmation-modal',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfirmationModalComponent implements OnInit {
 
-  constructor() { }
+  @Input() title: string;
+  @Input() description: string;
+  
+  constructor(public modal: NgbActiveModal) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
--- src/app/shared/components/shared-components.module.ts
+++ src/app/shared/components/shared-components.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 import { BasePaginationComponent } from './base-pagination/base-pagination.component';
 import { ConfirmationModalComponent } from './confirmation-modal/confirmation-modal.component';
 
+const components = [
+  ConfirmationModalComponent
+];
+
 @NgModule({
-  declarations: [
-    ConfirmationModalComponent
-  ],
+  declarations: components,
   imports: [
     CommonModule
-  ]
+  ],
+  exports: components
 })
 export class SharedComponentsModule { }
