fab2ac8879d38ff6d28be35c2f6ab98143220c72
Neven Falica <neven.falica@fleksbit.hr>
2020-06-19 16:56:47 +0200

    Diagrams WIP

--- src/app/diagram/diagram-overview/diagram-overview.component.ts
+++ src/app/diagram/diagram-overview/diagram-overview.component.ts
@@ -7,93 +7,113 @@ import { Network, DataSet, Node, Edge, IdType } from 'vis'
   styleUrls: ['./diagram-overview.component.scss']
 })
 export class DiagramOverviewComponent implements OnInit, OnDestroy {
-    nodes: Node = new DataSet([
-      {id: 1, label: 'TCC'},
-      {id: 2, label: 'Armaghast'},
-      {id: 3, label: 'Maui-Covenant'},
-      {id: 4, label: 'Nordholm'},
-      {id: 5, label: 'Bressia'},
-      {id: 6, label: 'Pacem'},
-      {id: 7, label: 'Deneb Drei'},
-      {id: 8, label: 'SDS'},
-      {id: 9, label: 'Madhya'},
-      {id: 10, label: 'Hyperion'},
-    ]);
-    edges: Edge = new DataSet([
-      {from: 1, to: 8},
-      {from: 1, to: 3},
-      {from: 1, to: 4},
-      {from: 1, to: 5},
-      {from: 1, to: 6},
-      {from: 1, to: 7},
-      {from: 2, to: 10},
-      {from: 3, to: 9},
-      {from: 8, to: 2},
-    ]);
-    network: Network
-    options = {}
-    data
-    nodeActive: any = null
-    edgesActive: any = null
+  allUsers = [
+    {id: 101, name: 'Marko Markovic', oib: '12345', totalIn: 31950, totalOut: 115450},
+    {id: 102, name: 'Petar Petrovic', oib: '67890', totalIn: 0, totalOut: 0},
+    {id: 103, name: 'Dobra tvrtka d.o.o.', oib: '45678', totalIn: 0, totalOut: 0},
+  ]
+  allAccs = [
+    {id: 2001, userId: 101, accNo: 'HR100123', swift: 'ZABAHR2X', bank: 'ZABA', country: 'HR'},
+    {id: 2002, userId: 101, accNo: 'HR100987', swift: 'ZABAHR2X', bank: 'ZABA', country: 'HR'},
+    {id: 2003, userId: 101, accNo: 'HR200456', swift: 'PBZGHR2X', bank: 'PBZ', country: 'HR'},
+    {id: 2004, userId: 101, accNo: 'HR300333', swift: 'HPBZHR2X', bank: 'HPB', country: 'HR'},
+    {id: 2005, userId: 103, accNo: '123456', swift: 'BOFAUS3N', bank: 'BOFA', country: 'US'},
+  ]
+  allTrans = [
+    {id: 30001, accId: 2001, transaction: 'Uplata', amount: 3500, direction: 1},
+    {id: 30002, accId: 2001, transaction: 'Isplata', amount: 5000, direction: -1},
+    {id: 30003, accId: 2001, transaction: 'Isplata', amount: 3250, direction: -1},
+    {id: 30004, accId: 2002, transaction: 'Isplata', amount: 25000, direction: -1},
+    {id: 30005, accId: 2003, transaction: 'Uplata', amount: 10000, direction: 1},
+    {id: 30006, accId: 2003, transaction: 'Uplata', amount: 7000, direction: 1},
+    {id: 30007, accId: 2003, transaction: 'Uplata', amount: 1500, direction: 1},
+    {id: 30008, accId: 2003, transaction: 'Uplata', amount: 1250, direction: 1},
+    {id: 30009, accId: 2003, transaction: 'Isplata', amount: 74100, direction: -1},
+  ]
 
-    public constructor() {}
-    public ngOnInit(): void {
-      const container = document.getElementById('network')
-      this.data = {
-        nodes: this.nodes,
-        edges: this.edges
-      }
-      this.network = new Network(container, this.data, this.options)
+  nodes: Node = new DataSet([])
+  edges: Edge = new DataSet([])
+  network: Network
+  options = {}
+  data
+  nodeActive: any = null
+  edgesActive: any = null
 
-      this.network.on('selectNode', ctx => { this.selectNode(ctx) })
-      this.network.on('deselectNode', ctx => { this.deselectNode(ctx) })
-      this.network.on('dragEnd', ctx => { this.selectNode(ctx) })
+  public constructor() {}
+  public ngOnInit(): void {
+    const container = document.getElementById('network')
+    this.data = {
+      nodes: this.nodes,
+      edges: this.edges
     }
-    public ngOnDestroy(): void {}
+    this.network = new Network(container, this.data, this.options)
 
-    selectNode(ctx) {
-      let idx = ctx.nodes[0]
-      let linksTo = this.edges.get({
-        filter: (itm) => { return itm.to === idx }
+    console.log('ALLACCS', this.allAccs)
+
+    this.network.on('selectNode', ctx => { this.selectNode(ctx) })
+    this.network.on('deselectNode', ctx => { this.deselectNode(ctx) })
+    this.network.on('dragEnd', ctx => { this.selectNode(ctx) })
+  }
+  public ngOnDestroy(): void {}
+
+  selectNode(ctx) {
+    let idx = ctx.nodes[0]
+    this.nodeActive = this.nodes.get(idx)
+    console.log('Selected Node', idx, this.nodeActive)
+    if (this.nodeActive.type === 'user') {
+      this.nodeActive.user = this.allUsers.find(itm => { return itm.id === this.nodeActive.id })
+      this.nodeActive.accounts = []
+      this.nodeActive.accounts.push(...this.allAccs.filter(itm => { return itm.userId === this.nodeActive.user.id }))
+      let ai = 0
+      this.nodeActive.accounts.forEach(aitm => {
+        this.nodeActive.accounts[ai].transactions = []
+        this.nodeActive.accounts[ai].transactions.push(...this.allTrans.filter(titm => { return titm.accId === aitm.id }))
+        ai++
       })
-      let linksFrom = this.edges.get({
-        filter: (itm) => { return itm.from === idx }
+    } else if (this.nodeActive.type === 'account') {
+      let acc = this.allAccs.find(itm => { return itm.id === this.nodeActive.id })
+      this.nodeActive.user = this.allUsers.find(itm => { return itm.id === acc.userId })
+      this.nodeActive.account = acc
+      this.nodeActive.account.transactions = []
+      this.nodeActive.account.transactions.push(...this.allTrans.filter(titm => { return titm.accId === acc.id }))
+    }
+    console.log('Selected Node', idx, this.nodeActive)
+  }
+  deselectNode(ctx) {
+    this.nodeActive = null
+    console.log('Deselected Node')
+  }
+  controlNodeDragEnd(ctx) {
+    console.log('dragStart', ctx)
+  }
+
+  addUser() {
+    const person = this.allUsers[0]
+    this.nodes.update({
+      id: person.id,
+      label: person.name + '\r\nOIB: ' + person.oib,
+      type: 'user'
+    })
+
+    const accsList = this.allAccs.filter(itm => { return itm.userId === person.id })
+    accsList.forEach(itm => {
+      this.nodes.update({
+        id: itm.id,
+        label: itm.accNo + '\r\n' + itm.swift,
+        type: 'account'
       })
-      this.nodeActive = this.nodes.get(idx)
-      this.nodeActive.connections = linksTo.length + linksFrom.length
-      console.log('Selected Node', idx, this.nodeActive)
-      console.log('Connects to', linksTo.length, linksTo)
-      console.log('Connects from', linksFrom.length, linksFrom)
-    }
-    deselectNode(ctx) {
-      this.nodeActive = null
-      console.log('Deselected Node')
-    }
-    controlNodeDragEnd(ctx) {
-      console.log('dragStart', ctx)
-    }
+      this.edges.update({
+        from: person.id,
+        to: itm.id
+      })
+    })
+  }
 
-    addNode() {
-      const rnd = this.rndmm(20, 100)
-      let newNode = {
-        id: rnd,
-        label: 'Node ' + rnd
-      }
-      this.nodes.update(newNode)
+  removeNode() {
+    console.log('Remove Node', this.nodeActive)
+  }
 
-      let newEdge = {
-        from: this.rndmm(1, 10),
-        to: rnd
-      }
-      this.edges.update(newEdge)
-      console.log('New Node', newNode, newEdge)
-    }
-
-    removeNode() {
-      console.log('Remove Node', this.nodeActive)
-    }
-
-    rndmm(min, max) {
-      return Math.floor(Math.random() * (max - min + 1) + min)
-    }
+  rndmm(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
 }
