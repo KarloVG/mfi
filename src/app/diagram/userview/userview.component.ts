68e9cb7c80c59168ca7f74976f23305bdfe00f38
Neven Falica <neven.falica@fleksbit.hr>
2020-09-10 17:11:39 +0200

    Dijagram WIP

--- src/app/diagram/userview/userview.component.ts
+++ src/app/diagram/userview/userview.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {untilComponentDestroyed} from '@w11k/ngx-componentdestroyed';
 import { DiagramService } from 'src/app/shared/services/diagram.service'
+import {ModalBaseDetailComponent} from 'src/app/statement-base/base-overview/modal-base-detail/modal-base-detail.component'
 
 @Component({
   selector: 'app-userview',
@@ -23,7 +26,10 @@ export class UserviewComponent implements OnInit {
 
   areDetailsExpanded: boolean = false
 
-  constructor(private diaSvc: DiagramService) {}
+  constructor(
+    private diaSvc: DiagramService,
+    private ngbModalService: NgbModal
+  ) {}
   ngOnInit(): void {
     this.setup()
   }
@@ -65,7 +71,24 @@ export class UserviewComponent implements OnInit {
     this.areDetailsExpanded = false
   }
 
+  showAccountInfo(brRacuna) {
+    console.log('EEK', brRacuna)
+    console.log('DTX', this.node.data.izvodi)
+    //const izvod = this.node.data.izvodi.find(itm => { console.log('->', itm); return itm.brojRacuna == brRacuna })
+    const izvodi = this.node.data.izvodi.map(itm => { return itm.izvodID })
+    console.log('RX', izvodi)
+    this.diaSvc.getDiagramAccountDetailList(izvodi, brRacuna).pipe(untilComponentDestroyed(this)).subscribe(
+      data => {
+        console.log('GDADx', data)
+        const modalRef = this.ngbModalService.open(ModalBaseDetailComponent, { size: 'xl', backdrop: 'static', keyboard: false, windowClass: 'largeModalClass' });
+        modalRef.componentInstance.izvod = data
+      }
+    )
+  }
+
   closeInfobox() {
     this.close.emit(true)
   }
+
+  public ngOnDestroy(): void {}
 }
